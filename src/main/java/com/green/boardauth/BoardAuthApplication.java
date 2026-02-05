55e8191a6b1af1f5c651fbfab3d80a0daf98bce7
soyoung <ssssy93@icloud.com>
2026-02-05 09:56:51 +0900

    init: board auth backend project

---
+++ src/main/java/com/green/boardauth/BoardAuthApplication.java
@@ -0,0 +1,13 @@
+package com.green.boardauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardAuthApplication.class, args);
+    }
+
+}
